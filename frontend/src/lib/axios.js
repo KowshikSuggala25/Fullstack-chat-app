66c358e83400e00de11d7b5bf38a9a3f4225efac
SAI KOWSHIK SUGGALA <saikowshiksuggala9390@gmail.com>
2025-06-07 00:38:29 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000", // backend URL, update for production
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
   withCredentials: true,
 });
-
-export default axiosInstance;
