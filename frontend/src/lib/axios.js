58fb283b0f33a86f28301a758bd5fa1ae4715775
SAI KOWSHIK SUGGALA <saikowshiksuggala9390@gmail.com>
2025-06-22 15:20:43 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,16 @@
 import axios from "axios";
 
+const token = localStorage.getItem("token");
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+};
+
+const res = await axios.get("/api/messages/users", config);
+
+
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
   withCredentials: true,
