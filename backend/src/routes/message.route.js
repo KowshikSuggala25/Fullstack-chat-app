6ec42bb42f4941e4fa13b9f72bd1d0ab7ae2afbf
SAI KOWSHIK SUGGALA <saikowshiksuggala9390@gmail.com>
2025-06-27 20:37:34 +0530

    Update message.route.js

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware.js";
-import { sendMessage, getMessages, getUsers } from "../controllers/message.controller.js";
+import {
+  getUsersForSidebar,
+  getMessages,
+  sendMessage
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/users", protectRoute, getUsers);
-router.get("/:id", getMessages);
-router.post("/send/:id", sendMessage);
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
