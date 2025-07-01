a45a243532ee6573e69756b734cfc4e6f393738d
SAI KOWSHIK SUGGALA <saikowshiksuggala9390@gmail.com>
2025-07-01 21:30:12 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,6 +31,7 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use('/uploads', express.static('uploads'));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
