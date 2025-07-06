9d74f71cb73b8abfca8e40232b8dd4d6ea750378
SAI KOWSHIK SUGGALA <saikowshiksuggala9390@gmail.com>
2025-07-06 20:47:16 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -8,10 +8,12 @@ const HomePage = () => {
   const { selectedUser } = useChatStore();
 
   return (
-    <div className="h-screen w-screen bg-base-200 flex items-center justify-center px-4">
-      <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-full flex overflow-hidden">
-        <Sidebar />
-        {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+    <div className="min-h-screen w-full bg-base-200 flex items-center justify-center px-4 py-4">
+      <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl max-h-screen flex flex-col overflow-hidden">
+        <div className="flex flex-1 overflow-hidden">
+          <Sidebar />
+          {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+        </div>
       </div>
     </div>
   );
