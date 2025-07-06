2e106777086c1f7d62c20603aa956cfe7796ff05
SAI KOWSHIK SUGGALA <saikowshiksuggala9390@gmail.com>
2025-07-06 22:07:33 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,30 +1,24 @@
 import { useChatStore } from "../store/useChatStore";
+
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
 
 const HomePage = () => {
-  const { selectedUser, isSidebarOpen, setIsSidebarOpen } = useChatStore();
+  const { selectedUser } = useChatStore();
 
   return (
-    <div className="min-h-screen w-full bg-base-200 flex items-center justify-center px-4 py-4 overflow-hidden">
-      <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl max-h-screen flex flex-col lg:flex-row overflow-hidden">
-        
-        {(isSidebarOpen || !selectedUser) && (
-          <Sidebar />
-        )}
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
 
-        {selectedUser && (
-          <ChatContainer onClose={() => setIsSidebarOpen(true)} />
-        )}
-
-        {!selectedUser && !isSidebarOpen && (
-          <NoChatSelected />
-        )}
-
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
       </div>
     </div>
   );
 };
-
 export default HomePage;
