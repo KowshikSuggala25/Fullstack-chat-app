2a4ca2f816f441951625795e86b59d1b4d26de6c
SAI KOWSHIK SUGGALA <saikowshiksuggala9390@gmail.com>
2025-07-20 00:14:05 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://fullstack-chat-app-pb32.onrender.com/api",
   withCredentials: true,
 });
