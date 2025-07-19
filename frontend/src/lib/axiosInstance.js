a8ce1f506b12ed74db61e9a67782608db3f6cea5
SAI KOWSHIK SUGGALA <saikowshiksuggala9390@gmail.com>
2025-07-20 00:24:41 +0530

    Create axiosInstance.js

---
+++ frontend/src/lib/axiosInstance.js
@@ -0,0 +1,9 @@
+import axiosInstance from "axiosInstance";
+
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "https://fullstack-chat-app-pb32.onrender.com/api",
+  withCredentials: true,
+});
