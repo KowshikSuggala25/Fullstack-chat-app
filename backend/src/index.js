83383cce834788150f13ae65b06a6595a0105598
SAI KOWSHIK SUGGALA <saikowshiksuggala9390@gmail.com>
2025-07-21 23:00:06 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -39,10 +39,10 @@ app.use("/api/messages", messageRoutes);
 console.log('Routes mounted.');
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
   
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
   });
 }
 
